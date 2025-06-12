d312e26a7f04f7e3f5b13de08422f4ad6825786a
Krishna Mohan <krishanmohank974@gmail.com>
2025-06-12 14:24:54 +0530

    crud for website

--- apps/api/routes/v1/website.ts
+++ apps/api/routes/v1/website.ts
@@ -1,14 +1,22 @@
 import { Router } from "express";
-
+import prismaClient from "store/client";
+import express from "express";
 const websitesRouter = Router();
 
+websitesRouter.use(express.json());
 
-websitesRouter.get("/status/:websiteId", (req, res) => {
-    res.send("Hello World");
+websitesRouter.get("/status/:websiteId", async (req, res) => {
+  await prismaClient.website.create({
+    data: {
+      url: req.body.url,
+      timeAdded: new Date(),
+      region: req.body.region,
+    },
   });
-  
-  websitesRouter.post("/website", (req, res) => {
-    res.send("Hello World");
-  });
+});
 
-  export default websitesRouter;
+websitesRouter.post("/website", (req, res) => {
+  res.send("Hello World");
+});
+
+export default websitesRouter;
